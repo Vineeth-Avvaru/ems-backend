bf3ac1a2edf5a3443a8d095f54df325f649b4ab5
Vineeth Avvaru <avvaru95@gmail.com>
2020-06-26 13:24:36 +0900

    CORS for PUT, DELETE

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const mysql = require('mysql');
 app.use(function(req, res, next){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 
